cb424c1874a145b1fe55dc3eedba753c85f28029
pq93 <pq93@foxmail.com>
2019-08-02 00:17:22 +0200

    Update linear regression exercise

--- Assignment_03/LRExercise.py
+++ Assignment_03/LRExercise.py
@@ -9,6 +9,14 @@ def inference(w, b, x):
     pred_y = w * x + b
     return pred_y
 
+# Evaluation
+def eval_loss(w, b, x_list, gt_y_list):
+    avg_loss = 0.0
+    for i in range(len(x_list)):
+        avg_loss += 0.5 * (w * x_list[i] + b - gt_y_list[i]) ** 2
+    avg_loss /= len(gt_y_list)
+    return avg_loss
+
 # Gradient: get dw & db
 def gradient(pred_y, gt_y, x):
     """
@@ -39,10 +47,43 @@ def cal_step_gradient(batch_x_list, batch_gt_y_list, w, b, lr):
     return w, b
 
 def train(x_list, gt_y_list, batch_size, lr, max_iter):
-    w, b = 0
+    w, b = 0, 0
     num_samples = len(x_list)
     for i in range(max_iter):
         # SGD: Stochastic Gradient Descent (applicable for batch_size = 1)
         ## Minibatch SGD if batch_size > 1
         ### Gradient Descent if batch_size = num_samples
         batch_idxs = np.random.choice(len(x_list), batch_size) # Select batchsize samples from 0 to numsamples
+        batch_x = [x_list[j] for j in batch_idxs]
+        batch_y = [gt_y_list[j] for j in batch_idxs]
+        w, b = cal_step_gradient(batch_x, batch_y, w, b, lr)
+
+        print('w:{0}, b:{1}'.format(w, b))
+        print('Loss is {0}'.format(eval_loss(w, b, x_list, gt_y_list)))
+
+        # return None
+
+# Generate sample data
+def gen_sample_data():
+    w = random.randint(0, 10) + random.random()
+    b = random.randint(0, 5) + random.random()
+    num_samples = 100
+    x_list = []
+    y_list = []
+    for i in range(num_samples):
+        x = random.randint(0, 100) * random.random()
+        y = w * x + b + random.random() * random.randint(-1, 1)
+        x_list.append(x)
+        y_list.append(y)
+
+    return x_list, y_list, w, b
+
+def run():
+    x_list, y_list, w, b = gen_sample_data()
+    lr = 0.001 # if lr too big => gradient exploration => loss NaN
+    max_iter = 1000
+    train(x_list, y_list, 50, lr, max_iter)
+    return None
+
+if __name__ == '__main__':
+    run()
