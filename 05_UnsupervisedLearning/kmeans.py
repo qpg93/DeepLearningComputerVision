7ea0d0c8d45161dc076910629a22e9be044525fc
pq93 <pq93@foxmail.com>
2019-09-12 02:51:14 +0200

    Update Kmeans

---
+++ 05_UnsupervisedLearning/kmeans.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+def assignment(df, centroids, colmap):
+    pass
+
+def update(df, centroids):
+    pass
+
+def main():
+    df = pd.DataFrame({
+        'x': [12, 20, 28, 18, 10, 29, 33, 24, 45, 45, 52, 51, 52, 55, 61],
+        'y': [ddd]
+    })
+
+
+if __name__ == '__main__':
+    main()
